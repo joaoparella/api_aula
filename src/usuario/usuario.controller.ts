6aed70c4710d72278026acfe26089cd1956f104d
joao pedro <joao.pparella@senacsp.edu.br>
2025-09-04 22:25:17 -0300

    Alterações 04/09/2025 Criando decorator para validação do email unico Criando Métodos do CRUD

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,8 +1,10 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
 import { UsuarioEntity } from "./usuario.entity";
 import { UsuariosArmazenados } from "./usuario.dm";
 import {v4 as uuid} from 'uuid';
 import { CriaUsuarioDTO } from "./dto/criaUsuario.dto";
+import { ListaUsuarioDTO } from "./dto/listaUsuario.dto";
+import { AlteraUsuarioDTO } from "./dto/alteraUsuario.dto";
 
 @Controller('/usuarios')
 export class UsuarioController {
@@ -23,5 +25,37 @@ export class UsuarioController {
         message: 'Usuário criado com sucesso'
     };
     return retorno;
+  }
+
+  @Get()
+  async retornaUsuario(): Promise<ListaUsuarioDTO[]> {
+
+      var usuariosListados = this.Usuarios.Usuarios;
+      const ListaRetorno = usuariosListados.map(
+          usuario => new ListaUsuarioDTO(
+              usuario.id,
+              usuario.nome,
+              usuario.email
+          )
+      );
+      return ListaRetorno;
+  }
+
+  @Put('/:id')
+  async alteraUsuario(@Param('id') id: string, @Body() dadosAtualizacao: AlteraUsuarioDTO) {
+      const usuarioAtualizado = await this.Usuarios.atualizaUsuario(id, dadosAtualizacao);
+      return {
+          usuario: usuarioAtualizado,
+          message: 'Usuário atualizado com sucesso'
+      };
+  }
+
+  @Delete('/:id')
+  async removeUsuario(@Param('id') id: string) {
+      const usuarioRemovido = await this.Usuarios.removeUsuario(id);
+      return {
+          usuario: usuarioRemovido,
+          message: 'Usuário removido com sucesso'
+      };
   }
 }
--- src/usuario/usuario.dm.ts
+++ src/usuario/usuario.dm.ts
@@ -6,10 +6,53 @@ export class UsuariosArmazenados{
     #usuarios: UsuarioEntity[] = [];  
 
     AdicionarUsuario(usuario: UsuarioEntity){
-        this.#usuarios.push(usuario);
+            this.#usuarios.push(usuario);
+        
     }
 
+    async validaEmail(email: string): Promise<boolean>{
+        const usuarioEncontrado = this.#usuarios.find(u => u.email === email);
+        return usuarioEncontrado !== undefined;
+    }
+
+    private BuscaPorID(id: string): UsuarioEntity {
+        const possivelUsuario = this.#usuarios.find(
+            usuarioSalvo => usuarioSalvo.id === id
+        );
+
+        if (!possivelUsuario) {
+            throw new Error('Usuário não encontrado');
+        }
+        return possivelUsuario;
+    }
+
+    async removeUsuario(id: string) {
+        const usuario = this.BuscaPorID(id);
+
+        this.#usuarios = this.#usuarios.filter(
+            usuarioSalvo => usuarioSalvo.id !== id
+        );
+
+        return usuario;
+    }
  
+    async atualizaUsuario(id: string, dadosAtualizacao: Partial<UsuarioEntity>) {
+        var possivelUsuario = this.BuscaPorID(id);
+
+        Object.entries(dadosAtualizacao).forEach(
+            ([chave, valor]) => {
+                if (chave === 'id') {
+                    return;
+                }else if (valor === undefined) {
+                    return;
+
+                }
+                possivelUsuario[chave] = valor;
+            }
+        );
+
+        return possivelUsuario;
+    }
 
     get Usuarios(){        
         return this.#usuarios;
